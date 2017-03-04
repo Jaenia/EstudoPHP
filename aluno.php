954b030d44bbdf228aa4b38af391fb38aa6b572b
rcardosoo <rafaelalbuquerquecardoso@gmail.com>
2017-03-04 17:12:30 -0300

    estrutura basica

---
+++ aluno.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php //conexao PDO ?>
+
+<?php //receber aluno selecionado e mostrar detalhes ?>
+<?php //atualizar aluno ?>
+<?php //deletar aluno ?>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto PHP</title>
+</head>
+<body>
+
+<?php //conexao PDO	?>
+
+<?php //criacao das tabelas ?>
+
+<!-- Formulario para cadastro -->
+
+<?php //receber dados e cadastrar ?>
+
+<!-- Formulário p/ listar alunos de uma disciplina -->
+
+<?php //receber dados e listar como link (ir para aluno.php) ?>
+
+
+
+
+</body>
+</html>
